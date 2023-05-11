c57ab931f9a72e8dc5d8c5fc56f5e6196d0cc06d
Rishabh0319 <rishabhtripathi0319@gmail.com>
2023-05-11 20:50:57 +0530

    on project {spotify-clone-reactjs} = Page Layout Design is done including all Hadder, Footer, Sidebar and MainBody

--- spotify-clone-reactjs/src/components/Spotify.jsx
+++ spotify-clone-reactjs/src/components/Spotify.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+import Body from './Body'
+import Footer from './Footer'
 
 const Spotify = () => {
   return (
-    <div>Spotify</div>
+    <div className='container'>
+      <div className="spotify__body">
+        <Sidebar />
+        <div className="body">
+          <Navbar />
+          <div className="body__contents">
+            <Body />
+          </div>
+        </div>
+      </div>
+      <div className="spotify__footer">
+        <Footer />
+      </div>
+    </div>
   )
 }
 
