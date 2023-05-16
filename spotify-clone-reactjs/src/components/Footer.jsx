7fa83d5fc07e8f2148287d05f5e5c642a2e2a28e
Rishabh0319 <rishabhtripathi0319@gmail.com>
2023-05-16 19:33:53 +0530

    on project {spotify-clone-reactjs} = volume controle Component functionality and Design is Done

--- spotify-clone-reactjs/src/components/Footer.jsx
+++ spotify-clone-reactjs/src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import CurrentTrack from './CurrentTrack'
 import PlayerControle from './PlayerControle'
+import Volume from './Volume'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <CurrentTrack/>
       <PlayerControle/>
+      <Volume/>
     </div>
   )
 }
