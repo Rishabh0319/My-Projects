0159c106e938231fb428ffc2ed03a5231a72e9b7
Rishabh0319 <rishabhtripathi0319@gmail.com>
2023-05-22 09:28:57 +0530

    on project {google-colone-reactjs} = search component design and basic styling is done

--- google-clone-reactjs/src/Components/ProfileIcons.jsx
+++ google-clone-reactjs/src/Components/ProfileIcons.jsx
@@ -5,7 +5,7 @@ const ProfileIcons = () => {
   return (
     <div className='profile-icon-cont'>
        <span><TbGridDots size={25} color='#5f6368'/></span> 
-       <span className='profile'><img src='https://lh3.google.com/u/0/ogw/AOLn63H0P9mZLWcqdD_ahUhstHLlvpF0fOfX1IDXIviD=s32-c-mo' alt="profile-image" /></span>
+       <span className='profile'><img src='https://lh3.google.com/u/0/ogw/AOLn63H0P9mZLWcqdD_ahUhstHLlvpF0fOfX1IDXIviD=s32-c-mo' alt="profile" /></span>
     </div>
   )
 }
--- google-clone-reactjs/src/Components/SearchInput.jsx
+++ google-clone-reactjs/src/Components/SearchInput.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import MicIcon from '../assets/mic.svg';
+import ImageIcon from '../assets/image.svg';
+import { IoMdClose } from 'react-icons/io';
 
 const SearchInput = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
-    <input type="text" placeholder='Search'/>
+    <div className="searchBox">
+      <AiOutlineSearch size={20} />
+      <input
+        type="text"
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyUp={(e) => console.log(e.target.value)}
+        placeholder='Search'
+        value={searchQuery} />
+      <div className="search-icons-utl">
+        {
+          searchQuery && (
+            <IoMdClose
+              size={24}
+              color="#70757a"
+              className="cursor-pointer"
+              onClick={() => setSearchQuery("")}
+            />
+          )
+        }
+        <img src={MicIcon} alt="Mic" />
+        <img src={ImageIcon} alt="cam-icon" />
+      </div>
+    </div>
   )
 }
 
