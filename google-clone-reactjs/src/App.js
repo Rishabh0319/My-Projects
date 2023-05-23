3b36e3523b49bc2f3360f2afec8dbf14e489caf5
Rishabh0319 <rishabhtripathi0319@gmail.com>
2023-05-23 13:11:44 +0530

    global state setup of context API is Done

--- google-clone-reactjs/src/App.js
+++ google-clone-reactjs/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import SearchReasult from './Components/SearchReasult';
 
+import {AppContext} from './utils/Context.js';
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-           <Route path='/' exact element={<Home/>}/>
-           <Route path='/:query/:startIndex' element={<SearchReasult/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <AppContext>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path='/:query/:startIndex' element={<SearchReasult />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AppContext>
   );
 }
 
--- google-clone-reactjs/src/Components/SearchReasultHeader.jsx
+++ google-clone-reactjs/src/Components/SearchReasultHeader.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import LOGO from '../assets/google-logo.png';
 import SearchInput from './SearchInput';
 import ProfileIcons from './ProfileIcons';
 import { menu } from '../utils/Constants';
+import { Context } from '../utils/Context.js';
+import { Link } from 'react-router-dom';
 
 const SearchReasultHeader = () => {
 
     const [selectedMenu, SetSelectedState] = useState('All');
+    const { setImageSearch } = useContext(Context);
+
+    useEffect(() => {
+        // to reset the state of setImageSearch
+        return () => setImageSearch(false);
+    }, [])
 
     const clickHandler = (menu) => {
-       console.log(menu);
-       SetSelectedState(menu.name);
+        let isTypeImage = menu.name === "Images";
+        SetSelectedState(menu.name);
+        setImageSearch(isTypeImage ? true : false);
     }
 
     return (
         <div className='searchreasultheader-cont'>
             <div className='inner-div'>
                 <div className='inner-div-child'>
-                    <img className='SR-header-logo' src={LOGO} alt="" />
+                    <Link to="/">
+                        <img className='SR-header-logo' src={LOGO} alt="" />
+                    </Link>
                     <SearchInput />
                 </div>
                 <div>
@@ -30,7 +41,7 @@ const SearchReasultHeader = () => {
                         <span
                             className='menu-links'
                             key={index}
-                            onClick={() => {clickHandler(menu)}}
+                            onClick={() => { clickHandler(menu) }}
                             style={{
                                 color: (selectedMenu === menu.name) ? "#1a73e8" : "#5f6368",
                             }}
@@ -38,11 +49,11 @@ const SearchReasultHeader = () => {
 
                             <span>{menu.icon}</span>
                             <span className='active-menu'>{menu.name}
-                            {selectedMenu === menu.name && (
-                                <span className='active-menu-underline'></span>
-                            )}
+                                {selectedMenu === menu.name && (
+                                    <span className='active-menu-underline'></span>
+                                )}
                             </span>
-                            
+
                         </span>
                     ))
                 }
