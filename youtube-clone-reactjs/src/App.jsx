4b807ee1b4f73341fe61bfe82c0c2f6b60e15988
Rishabh0319 <rishabhtripathi0319@gmail.com>
2023-06-09 17:56:19 +0530

    in project { Youtube-clone-reactjs } = Header Section Design and functionality is Done All Works fine :)

--- youtube-clone-reactjs/src/App.jsx
+++ youtube-clone-reactjs/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import "./App.css";
+//importing Components
+import Header from "./Components/Header.jsx";
+import Feed from "./Components/Feed.jsx";
+import SearchResult from './Components/SearchResult.jsx';
+import VideoDetailes from "./Components/VideoDetailes.jsx";
+
+// React Routes (RRD)
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Context API
 import { AppContext } from './Context/contextApi';
+
+
 
 const App = () => {
     return (
         <AppContext>
-            <div className='text-3xl text-green-600'>App</div>
+            <BrowserRouter>
+                <div className='flex flex-col h-full'>
+                   <Header/>
+                   <Routes>
+                     <Route path='/' exact element={<Feed/>}/>
+                     <Route path='/searchResult/:searchQuery' element={<SearchResult/>}/>
+                     <Route path='/video/:id' element={<VideoDetailes/>}/>
+                   </Routes>
+                </div>
+            </BrowserRouter>
         </AppContext>
     )
 }
---
+++ youtube-clone-reactjs/src/Components/SearchReasultVideoCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchReasultVideoCard = () => {
+  return (
+    <div>SearchReasultVideoCard</div>
+  )
+}
+
+export default SearchReasultVideoCard
