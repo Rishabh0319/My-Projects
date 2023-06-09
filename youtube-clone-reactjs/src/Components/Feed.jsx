308cd552d1d3715cd39d6a01c2edd9941b5f41b5
Rishabh0319 <rishabhtripathi0319@gmail.com>
2023-06-09 21:28:33 +0530

    in Project {Youtube-clone-reactjs} = Sidebar Design and Selected Categoeryes functionality is Done All Clear without any error :)

--- youtube-clone-reactjs/src/Components/Feed.jsx
+++ youtube-clone-reactjs/src/Components/Feed.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { Context } from '../Context/contextApi';
+import LeftNav from "./LeftNav";
 
 const Feed = () => {
+  useContext(Context);
+  // useEffect();
   return (
-    <div>Feed</div>
+    <div className='flex flex-row h-[calc(100%-56px)]'>
+      <LeftNav />
+    </div>
   )
 }
 
